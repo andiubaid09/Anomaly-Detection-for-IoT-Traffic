d47991f885fd560725d6f3748eb4f896c198d983
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-11-24 11:29:25 +0800

    Add Preprocessing.py

--- KMeans/src/Preprocessing.py
+++ KMeans/src/Preprocessing.py
@@ -4,3 +4,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.cluster import KMeans
 
+drive.mount("/content/drive")
+datasheet = "/content/drive/My Drive/Datasheet/DDoS/unlabeled_data.csv"
+df = pd.read_csv(datasheet)
+df.head(5)
+df.describe()
+df.drop(columns='Unnamed: 0', inplace=True)
+df.head(2)
